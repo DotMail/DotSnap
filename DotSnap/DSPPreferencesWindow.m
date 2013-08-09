09446ef01851b0a122ff77deb52789141484fc65
CodaFi <devteam.codafi@gmail.com>
2013-08-08 19:44:32 -0600

    Fix settings window black arrow

--- DotSnap/DSPPreferencesWindow.m
+++ DotSnap/DSPPreferencesWindow.m
@@ -14,15 +14,15 @@
 
 @implementation DSPPreferencesWindow
 
-- (MAAttachedWindow *)initWithView:(NSView *)view attachedToPoint:(NSPoint)point {
-	self = [super initWithView:view attachedToPoint:point];
+- (MAAttachedWindow *)initWithView:(NSView *)view attachedToPoint:(NSPoint)point onSide:(MAWindowPosition)side {
+	self = [super initWithView:view attachedToPoint:point onSide:side];
 	
 	self.cornerRadius = 0.f;
 	self.arrowHeight = 12.f;
 	self.borderWidth = 0.f;
+	self.arrowBaseWidth = 20.f;
 	self.borderColor = [NSColor colorWithCalibratedRed:0.260 green:0.663 blue:0.455 alpha:1.000];
 	self.backgroundColor = [NSColor colorWithCalibratedRed:0.260 green:0.663 blue:0.455 alpha:1.000];
-	self.arrowBaseWidth = 20.f;
 	
 	return self;
 }
