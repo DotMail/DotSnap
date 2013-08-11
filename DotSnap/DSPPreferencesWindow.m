f153126ac85c3d85c2588c96d18fd28e42dfa8f2
CodaFi <devteam.codafi@gmail.com>
2013-08-11 00:00:37 -0600

    Fixed preferences fade out

--- DotSnap/DSPPreferencesWindow.m
+++ DotSnap/DSPPreferencesWindow.m
@@ -24,6 +24,13 @@
 	self.borderColor = [NSColor colorWithCalibratedRed:0.260 green:0.663 blue:0.455 alpha:1.000];
 	self.backgroundColor = [NSColor colorWithCalibratedRed:0.260 green:0.663 blue:0.455 alpha:1.000];
 	
+	[NSNotificationCenter.defaultCenter addObserverForName:NSWindowDidResignKeyNotification object:self queue:nil usingBlock:^(NSNotification *note) {
+		[self orderOutWithDuration:0.3 timing:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut] animations:^(CALayer *layer) {
+			layer.transform = CATransform3DMakeTranslation(0.f, -50.f, 0.f);
+			layer.opacity = 0.f;
+		}];
+	}];
+	
 	return self;
 }
 
